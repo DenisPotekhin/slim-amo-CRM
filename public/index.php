a139775c7c682c136706d8eca51860095f23c1cd
Denis Potekhin <potekhin@umgoroda.ru>
2019-10-26 15:19:31 +0200

    Third commit

--- public/index.php
+++ public/index.php
@@ -9,23 +9,22 @@ $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-	#Массив с параметрами, которые нужно передать методом POST к API системы
-	$user = array(
-    	'USER_LOGIN' => 'alphaprint30@gmail.com', #Ваш логин (электронная почта)
-    	'USER_HASH' => 'b405c72a37e7cf46d75a7ead4936827b87e41b53', #Хэш для доступа к API (смотрите в профиле пользователя)
-	);
-	$subdomain = 'alphaprint30'; #Наш аккаунт - поддомен
-	#Формируем ссылку для запроса
-	$link = 'https://' . $subdomain . '.amocrm.ru/private/api/auth.php?type=json';
-	/* Нам необходимо инициировать запрос к серверу. Воспользуемся библиотекой cURL (поставляется в составе PHP). Вы также
-	можете
-	использовать и кроссплатформенную программу cURL, если вы не программируете на PHP. */
-	$curl = curl_init(); #Сохраняем дескриптор сеанса cURL
-	#Устанавливаем необходимые опции для сеанса cURL
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($curl, CURLOPT_USERAGENT, 'amoCRM-API-client/1.0');
-	curl_setopt($curl, CURLOPT_URL, $link);
-	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+
+$user = array(
+    'USER_LOGIN' => 'alphaprint30@gmail.com', #Ваш логин (электронная почта)
+    'USER_HASH' => 'b405c72a37e7cf46d75a7ead4936827b87e41b53', #Хэш для доступа к API (смотрите в профиле пользователя)
+);
+$subdomain = 'alphaprint30';
+$link = 'https://' . $subdomain . '.amocrm.ru/private/api/auth.php?type=json';
+/* Нам необходимо инициировать запрос к серверу. Воспользуемся библиотекой cURL (поставляется в составе PHP). Вы также
+можете
+использовать и кроссплатформенную программу cURL, если вы не программируете на PHP. */
+$curl = curl_init(); #Сохраняем дескриптор сеанса cURL
+
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_USERAGENT, 'amoCRM-API-client/1.0');
+curl_setopt($curl, CURLOPT_URL, $link);
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
 curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($user));
 curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 curl_setopt($curl, CURLOPT_HEADER, false);
@@ -60,10 +59,61 @@ try
 } catch (Exception $E) {
     die('Ошибка: ' . $E->getMessage() . PHP_EOL . 'Код ошибки: ' . $E->getCode());
 }
-	if ($code == 200 || $code == 204) {
-		$response->getBody()->write("Авторизация прошла успешно");
+
+$subdomain = 'alphaprint30'; #Наш аккаунт - поддомен
+/* Формируем ссылку для запроса */
+$link = 'https://' . $subdomain . '.amocrm.ru/api/v2/leads';
+
+$curl = curl_init();
+/* Устанавливаем необходимые опции для сеанса cURL */
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_USERAGENT, 'amoCRM-API-client/1.0');
+curl_setopt($curl, CURLOPT_URL, $link);
+curl_setopt($curl, CURLOPT_HEADER, false);
+curl_setopt($curl, CURLOPT_COOKIEFILE, dirname(__FILE__) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+curl_setopt($curl, CURLOPT_COOKIEJAR, dirname(__FILE__) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+
+/* Выполняем запрос к серверу. */
+$out = curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
+$code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+curl_close($curl);
+/* Теперь мы можем обработать ответ, полученный от сервера. Это пример. Вы можете обработать данные своим способом. */
+$code = (int) $code;
+$errors = array(
+    301 => 'Moved permanently',
+    400 => 'Bad request',
+    401 => 'Unauthorized',
+    403 => 'Forbidden',
+    404 => 'Not found',
+    500 => 'Internal server error',
+    502 => 'Bad gateway',
+    503 => 'Service unavailable',
+);
+try
+{
+    /* Если код ответа не равен 200 или 204 - возвращаем сообщение об ошибке */
+    if ($code != 200 && $code != 204) {
+        throw new Exception(isset($errors[$code]) ? $errors[$code] : 'Undescribed error', $code);
+    }
+} catch (Exception $E) {
+    die('Ошибка: ' . $E->getMessage() . PHP_EOL . 'Код ошибки: ' . $E->getCode());
+}
+/*
+Данные получаем в формате JSON, поэтому, для получения читаемых данных,
+нам придётся перевести ответ в формат, понятный PHP
+ */
+$Response = json_decode($out, true);
+$data = $Response['_embedded']['items'];
+$str = '';
+$i = 1;
+foreach ($data as $value) {
+$str = $str . 'Сделка №' . $i . ' - ' . $value['name'] . ' Ближайшая задача' . $value['closest_task_at'] . "<br />";
+$i += 1;
+}
+$response->getBody()->write($str);
 		return $response;
-	}
 	});
 
 $app->run();
