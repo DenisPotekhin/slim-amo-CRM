2d1776cc205fe1a4eb0dc3bfe720d5647a4abfbb
Denis Potekhin <potekhin@umgoroda.ru>
2019-10-26 15:52:04 +0200

    Firth commit

--- public/index.php
+++ public/index.php
@@ -119,6 +119,61 @@ foreach ($data as $value) {
 		$j +=1;
 	}
 }
+foreach ($data as $value) {
+	if ($value['closest_task_at'] == 0) {
+	$dealId = $value['id'];
+
+$tasks['add'] = array(
+    array(
+        'element_id' => $dealId, #ID сделки
+        'element_type' => 2, #Показываем, что это - сделка, а не контакт
+        'task_type' => 1, #Звонок
+        'text' => 'Сделка без задачи',
+        'responsible_user_id' => 109999,
+        'complete_till_at' => 1375285346,
+    ),
+);
+$subdomain = 'alphaprint30'; #Наш аккаунт - поддомен
+$link = 'https://' . $subdomain . '.amocrm.ru/api/v2/tasks';
+$curl = curl_init(); #Сохраняем дескриптор сеанса cURL
+#Устанавливаем необходимые опции для сеанса cURL
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_USERAGENT, 'amoCRM-API-client/1.0');
+curl_setopt($curl, CURLOPT_URL, $link);
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($tasks));
+curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+curl_setopt($curl, CURLOPT_HEADER, false);
+curl_setopt($curl, CURLOPT_COOKIEFILE, dirname(__FILE__) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+curl_setopt($curl, CURLOPT_COOKIEJAR, dirname(__FILE__) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+$out = curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
+$code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+/* Теперь мы можем обработать ответ, полученный от сервера. Это пример. Вы можете обработать данные своим способом. */
+$code = (int) $code;
+$errors = array(
+    301 => 'Moved permanently',
+    400 => 'Bad request',
+    401 => 'Unauthorized',
+    403 => 'Forbidden',
+    404 => 'Not found',
+    500 => 'Internal server error',
+    502 => 'Bad gateway',
+    503 => 'Service unavailable',
+);
+try
+{
+    #Если код ответа не равен 200 или 204 - возвращаем сообщение об ошибке
+    if ($code != 200 && $code != 204) {
+        throw new Exception(isset($errors[$code]) ? $errors[$code] : 'Undescribed error', $code);
+    }
+
+} catch (Exception $E) {
+    die('Ошибка: ' . $E->getMessage() . PHP_EOL . 'Код ошибки: ' . $E->getCode());
+}
+}
+
 	$response->getBody()->write('Сделки без задач' . "<br />" . $strWithoutTask . "<br />" . 'Сделки с задачами' . "<br />" . $strWithTask);
 		return $response;
 	});
